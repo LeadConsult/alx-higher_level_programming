2ae84e0120de7fe85606c6eeb4c5192fb0ed3f2a
LeadConsult <leadconsultnig@gmail.com>
2022-09-20 17:56:01 -0700

    task14

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    x = 0
-    new_str = ""
-    for ch in str:
-        if x != n:
-            new_str += ch
-        x += 1
-    return 
+    x = ""
+    for i in range(len(str)):
+        if i != n:
+            x = x + str[i]
+    return (x)
