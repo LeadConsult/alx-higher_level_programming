0ee96ccc4cd2b562d84707aecaa30842bac642d9
LeadConsult <leadconsultnig@gmail.com>
2022-09-22 16:27:57 -0700

    task8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,9 +2,9 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+        x = add(a, b)
+        for y in range(4, 6):
+            x = add(x, y)
+        return (x)
     else:
         return (sub(a, b))
