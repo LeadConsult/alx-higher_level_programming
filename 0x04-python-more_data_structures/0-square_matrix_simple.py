e1ce370ada70518bdbdec11e76ae4e2836da2975
LeadConsult <leadconsultnig@gmail.com>
2022-09-30 01:25:38 -0700

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix):
-    for i in range (0, len(matrix[0])):
-        for j in range (0, len(matrix[0])):
-            new_matrix[i][j] = matrix[i][j]*matrix[i][j]
-        
-    return(new_matrix)
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
