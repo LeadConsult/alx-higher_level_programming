968eda9affda7f371249ab1dd14e4e51c528376a
LeadConsult <leadconsultnig@gmail.com>
2022-11-06 16:52:31 -0800

    0x09-python-everything_is_object task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" 
+LockedClass class """
+
+class LockedClass():
+    """ 
+    variables declarations """
+    __slots__ = ('first_name')
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def copy_list(my_List):
+    return my_List[:]
