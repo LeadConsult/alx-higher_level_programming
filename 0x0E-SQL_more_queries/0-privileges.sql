3ddcf94e10b64b56bd3912d19d9731b8fdc35ab9
root <leadconsult@gmail.com>
2022-12-06 20:48:58 -0800

    ALL-combined-sql_query

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- This task is to lists all privileges of the users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This is alx task 0x0E-SQL_more_queries
